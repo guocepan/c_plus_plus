170c3385b6f723a6bffa000723fa0a4a527217ed
guocepan <1203543128@qq.com>
2019-10-13 10:22:25 +0800

    01 the first commint   forget class dog{ }; assighment.bad

---
+++ smile.cpp
@@ -0,0 +1,21 @@
+﻿#include "smile.h"
+int main()
+{
+
+	dog xiaohei(0,0);
+	dog xiaohong(8,8);
+	cout << xiaohong.sEye() << endl;
+
+	cout <<xiaohong.sLeg()<<endl;
+	cout << xiaohei.sEye();
+	cout << xiaohei.sLeg();
+
+	system("pause");
+
+
+
+
+
+
+	return 0;
+}
---
+++ smile.h
@@ -0,0 +1,38 @@
+﻿#ifndef _SMILE_
+#define _SMILE_
+
+#include<iostream>
+using namespace std;
+
+class dog; //declare
+ 
+class dog
+{
+public:
+	dog(int l = 4,int e =2):leg(l),eye(e)
+	{
+	}
+	int sLeg()
+	{
+		return leg;
+	}
+	int sEye()
+	{
+		return eye;
+	}
+
+
+private:
+  	int leg;
+	int eye;
+
+
+};//这个分号，手写的时候没有添加上去。
+
+
+
+
+
+
+
+#endif //_SMILE_
